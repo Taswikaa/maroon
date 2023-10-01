ea2192192d1ca0ce963325bbe48ad278923d2732
Alekseev Sergey <alekseev.s.v1@yandex.ru>
2023-10-01 17:39:31 +0300

    imported vendor

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      1
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi fuga nobis deleniti perferendis labore quis aliquid ratione quae recusandae! Nostrum dicta voluptas error sunt nihil sed placeat molestias quaerat blanditiis!
     </div>
   );
 }
